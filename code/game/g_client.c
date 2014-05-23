543187c51ecd8eae7bd8037e524db75b94a484a4
larukedi <eser@sent.com>
2014-05-23 23:44:10 +0300

    * machinegun starting ammo is equal in teamplay and ffa. * todo list.

--- code/game/g_client.c
+++ code/game/g_client.c
@@ -1156,12 +1156,7 @@ void ClientSpawn(gentity_t *ent) {
 	client->ps.clientNum = index;
 
 	client->ps.stats[STAT_WEAPONS] = ( 1 << WP_MACHINEGUN );
-	if ( g_gametype.integer == GT_TEAM ) {
-		client->ps.ammo[WP_MACHINEGUN] = 50;
-	} else {
-		// client->ps.ammo[WP_MACHINEGUN] = 100;
-        client->ps.ammo[WP_MACHINEGUN] = cpm_MGweapon; // CPM
-	}
+	client->ps.ammo[WP_MACHINEGUN] = cpm_MGweapon; // CPM
 
 	client->ps.stats[STAT_WEAPONS] |= ( 1 << WP_GAUNTLET );
 	client->ps.ammo[WP_GAUNTLET] = -1;
