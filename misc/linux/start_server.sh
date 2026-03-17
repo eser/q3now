dc0387a6b9464971e4780c9143bf51bb446a8b7d
Eser Ozvataf <eser@acikyazilim.com>
2026-03-18 00:12:04 +0300

    feat: add developer tooling (Makefile, modfiles, scripts)
    - Makefile: make build/pak/check/install/run/run-dev targets from
      archived-v1 (wraps cmake, packages zz-q3now.pk3, verifies outputs)
    - modfiles/: mod assets directory (autoexec.cfg, q3now mod identity)
    - misc/macos/start_server.sh: macOS server launcher
    - misc/linux/: Linux server scripts and service files
    - sync-upstream.sh: updated to track quake3e remote instead of ioquake3
      upstream; updated conflict guide for Quake3e sync workflow
    - cmake/basegame.cmake: add PREFIX "" to game library targets so dylibs
      are named cgame.dylib (not libcgame.dylib) to match engine expectations

---
+++ misc/linux/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Edit this script to change the path to q3now's dedicated server executable"
+echo "Set the sv_dlURL setting to a url like http://yoursite.com/q3now_path for q3now clients to download extra data."
+
+# sv_dlURL needs to have quotes escaped like \"http://yoursite.com/q3now_path\" or it will be set to "http:" in-game.
+
+~/q3now/q3now-ded +set dedicated 2 +set sv_allowDownload 1 +set sv_dlURL \"\" +set com_hunkmegs 64 "$@"
