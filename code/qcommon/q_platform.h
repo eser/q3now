83071ba40c69aef820dea0f41b1b53822409a429
Eser Ozvataf <eser@acikyazilim.com>
2026-03-22 06:20:02 +0300

    ci: github actions builds (take V).

--- code/qcommon/q_platform.h
+++ code/qcommon/q_platform.h
@@ -62,7 +62,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define PATH_SEP_FOREIGN '/'
 #define DLL_EXT ".dll"
 
-#if defined( _M_IX86 )
+#if defined( _M_IX86 ) || defined( __i386__ )
 #define ARCH_STRING "x86"
 #define Q3_LITTLE_ENDIAN
 #undef id386
@@ -72,7 +72,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 #endif
 
-#if defined( _M_AMD64 )
+#if defined( _M_AMD64 ) || defined( __x86_64__ ) || defined( __amd64__ )
 #define ARCH_STRING "x86_64"
 #define Q3_LITTLE_ENDIAN
 #undef idx64
